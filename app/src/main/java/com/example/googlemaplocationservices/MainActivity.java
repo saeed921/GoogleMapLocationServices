34a5252f12e3f35db8e57a60e2a1a2a88492b862
SaeedZ Pc <saeedofficial921@gmail.com>
2021-09-27 17:28:52 +0600

    get Latitude & Longitude

--- app/src/main/java/com/example/googlemaplocationservices/MainActivity.java
+++ app/src/main/java/com/example/googlemaplocationservices/MainActivity.java
@@ -1,17 +1,92 @@
 package com.example.googlemaplocationservices;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
+
+    FusedLocationProviderClient client;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
+    Double latitude;
+    Double longtitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int Googlemap;
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if(locationResult!=null){
+
+                    for(Location location:locationResult.getLocations()){
+
+                       latitude=  location.getLongitude();
+                       longtitude=location.getLongitude();
+
+                        Toast.makeText(MainActivity.this,latitude+""+
+                                longtitude+"",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+
+
+            }
+        };
+
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+CreateLocationRequest();
+
+    }
+
+    private void CreateLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(5000);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+ActivityCompat.requestPermissions(this,new String[]
+        {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},101);
+
+
+            return;
+
+        }
+
+        client.requestLocationUpdates(locationRequest, locationCallback, null);
+
+
 
 
     }
+
+
+
 }
